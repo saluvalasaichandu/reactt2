455e9b564abcaf5a26d19072bba645b06bca9ea7
saluvalasaichandu <saluvalasaichand@gmail.com>
2023-01-16 12:08:31 +0530

    react1

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return <>
+    <h2>Expense Items...</h2>
+     <h5>Food is Rs 10</h5>
+     <h5>Petrol is  Rs 100</h5>
+     <h5>Movies are Rs 200</h5>
+     </>
+
+      
+  
+}
+
+export default ExpenseItem;
